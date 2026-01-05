8dc7295c95230d1f14f4e1fdd73b8ab565a4417b
UdamShakya <udamilukpotha@gmail.com>
2026-01-05 23:47:55 +0530

    basics

---
+++ basics.py
@@ -0,0 +1,15 @@
+#strings 
+first_name = "bro"
+food = "pizza"
+print(f"hello {first_name}")
+print(f"you like {food}")
+
+#integers 
+age = 25
+print(f"you are {age} years old")
+quantity = 3
+print(f"you have ordered {quantity} {food}s")
+
+#
+
+ 
