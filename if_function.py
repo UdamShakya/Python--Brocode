3485cd418c3281b9d532cc76f6ee8e7beedb769a
UdamShakya <udamilukpotha@gmail.com>
2026-01-07 23:29:02 +0530

    Python Calculator

--- if_function.py
+++ if_function.py
@@ -31,3 +31,23 @@ if for_sale:
     print("The item is for sale.")
 else:
     print("The item is not for sale.")
+
+
+#python Calculator
+operator= input("Enter an operator (+, -, *, /): ")
+num1= float(input("Enter first number: "))
+num2= float(input("Enter second number: "))
+
+if operator== "+":
+    print(f"{num1} + {num2} = {num1 + num2}")
+elif operator== "-":
+    print(f"{num1} - {num2} = {num1 - num2}")
+elif operator== "*":
+    print(f"{num1} * {num2} = {num1 * num2}")
+elif operator== "/":
+    if num2 != 0:
+        print(f"{num1} / {num2} = {num1 / num2}")
+    else:
+        print("Division by zero error.")
+else:
+    print("Invalid operator.") 
