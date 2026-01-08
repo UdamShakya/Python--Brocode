cb58bac60d3719911cb394f048e664a69a66e4e0
UdamShakya <udamilukpotha@gmail.com>
2026-01-08 23:57:03 +0530

    String methods

--- string_methods.py
+++ string_methods.py
@@ -18,3 +18,38 @@ print(test)
 
 phone_number = input("Enter your phone number: ")
 phone_number.count("-")
+phone_number=phone_number.replace("-"," ")
+print(phone_number)
+
+
+#Exercise 1 validate user input exercise
+username= input("Enter username: ")
+
+if len(username) > 12:
+    print("Username must be under 12 characters.")
+elif not username.find(" ") == -1:
+    print("Username must not contain spaces.")
+elif not username.isalpha():
+    print("Username must only contain letters.")
+else:
+    print(f"Welcome, {username}!")
+
+
+#String Indexing
+
+credit_number= "1234-5678-9012-3456"
+
+print(credit_number[0])
+print(credit_number[0:5]) #Starting index is included ending index is not included
+print(credit_number[5:])
+print(credit_number[-1])
+
+last_digits= credit_number[-4:]
+
+print(f"**** **** **** {last_digits}")
+reverse= credit_number[::-1]
+print(reverse)
+
+#format specifiers
+
+
