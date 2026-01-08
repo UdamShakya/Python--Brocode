3a68eec8558ee3297d7a1cf7dae903e94f618a5f
UdamShakya <udamilukpotha@gmail.com>
2026-01-08 23:39:06 +0530

    String

---
+++ string_methods.py
@@ -0,0 +1,20 @@
+name = input("Enter your name: ")
+result = len(name)
+print(result)
+
+Result =name.find("u")
+Resultt=name.rfind("q")
+print(Result)
+print(Resultt)
+
+name = name.upper()
+name = name.lower()
+name = name.capitalize()
+print(name)
+
+test = name.isdigit()
+test= name.isalpha()
+print(test)
+
+phone_number = input("Enter your phone number: ")
+phone_number.count("-")
