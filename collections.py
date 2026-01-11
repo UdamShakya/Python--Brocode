ed56ab6703a8e6590ec7a1d2308c6dc53ba09f9d
UdamShakya <udamilukpotha@gmail.com>
2026-01-11 23:26:55 +0530

    shopping cart example

--- collections.py
+++ collections.py
@@ -48,3 +48,29 @@ print(dir(fruits_tuple))
 print(fruits.index("coconut"))
 
 print(fruits_tuple.count("coconut"))
+
+
+#Shopping cart program
+
+foods =[]
+prices =[]
+total = 0
+
+while True:
+    food= input("enter a food to buy (q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"enter the price of {food}: $"))
+        foods.append(food)
+        prices.append(price)
+
+print("----- Your cart------")
+
+for food in foods:
+    print(food, end=" ")
+
+for price in prices:
+    total += price
+
+print(f"\nTotal: ${total:.2f}")
