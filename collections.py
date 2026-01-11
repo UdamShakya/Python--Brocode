5fffe1d86e73fb40c625d77ec5f5aabc2ea33788
UdamShakya <udamilukpotha@gmail.com>
2026-01-11 23:15:15 +0530

    Sets

--- collections.py
+++ collections.py
@@ -1,24 +1,44 @@
-#lists
-fruits = ["apple", "banana", "cherry"]
+##list
+# fruits = ["apple", "banana", "cherry"]
 
-for fruit in fruits:
-    print(fruit)
+# for fruit in fruits:
+#     print(fruit)
 
-#print(dir(fruits))
+# #print(dir(fruits))
 
-fruits.append("orange")
-print(fruits)
+# fruits.append("orange")
+# print(fruits)
 
-print(len(fruits))
+# print(len(fruits))
 
-fruits.remove("apple")
-print(fruits)
-fruits.sort() #alphabetical order
-fruits.reverse() # reverse alphabetical order
-print(fruits)
-fruits.insert(0,"kiwi")
-print("pineapple" in fruits)
+# fruits.remove("apple")
+# print(fruits)
+# fruits.sort() #alphabetical order
+# fruits.reverse() # reverse alphabetical order
+# print(fruits)
+# fruits.insert(0,"kiwi")
+# print("pineapple" in fruits)
 
-print(fruits.index("banana"))
-print(fruits.count("kiwi")) # returns the count of "kiwi"
+# print(fruits.index("banana"))
+# print(fruits.count("kiwi")) # returns the count of "kiwi"
+
+# #Sets (unordered and immutable)
+# fruits_set = {"apple", "banana", "cherry"}
+# print(fruits_set)
+
+# print(dir(fruits_set))
+# #print(help(fruits_set))
+
+# fruits_set.add("orange")
+# print(fruits_set)
+
+# fruits_set.add("apple") # no effect, as sets do not allow duplicates
+# fruits_set.add("kiwi")
+
+# fruits_set.remove("banana")
+# fruits_set.pop()
+
+# print(fruits_set)
+
+#tuple
 
