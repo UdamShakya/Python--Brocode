9a96f0b412f7896197461a77979d56312f403f1d
UdamShakya <udamilukpotha@gmail.com>
2026-01-12 23:45:17 +0530

    Dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -4,3 +4,30 @@ Capitals  = {"usa": "Washington D.C.", "france": "Paris", "japan": "Tokyo","chin
 print(Capitals)
 print(dir(Capitals))
 print(Capitals["france"])
+#print(help(Capitals))
+
+if Capitals.get("usa"):
+    print("The capital of USA is", Capitals["usa"])
+else:
+    print("that capital doesn't exist")
+
+# Capitals.update({"germany": "Berlin"})
+# Capitals.pop("china")
+# Capitals.popitem()
+# Capitals.clear()
+
+print(Capitals)
+keys = Capitals.keys()
+
+for key in keys:
+    print(key)
+
+values = Capitals.values()
+
+for value in values:
+    print(value)
+
+items = Capitals.items()
+
+for key, value in items:
+    print(key, value)
