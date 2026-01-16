46abe856ed85199b620227436a74a42dab1ba38d
UdamShakya <udamilukpotha@gmail.com>
2026-01-16 22:40:04 +0530

    Random

--- Random_numbers.py
+++ Random_numbers.py
@@ -1,1 +1,23 @@
 import random
+#print(help(random))
+
+number=random.randint(1, 10)
+
+low=1 
+high=100
+
+num= random.randint(low, high)
+num2= random.random()
+
+options =("rock", "paper", "scissors")
+Cards =["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+option= random.choice(options)
+random.shuffle(Cards)
+
+print(Cards)
+
+print(num2)
+
+print("Random number between 1 and 10:", number)
+
+
