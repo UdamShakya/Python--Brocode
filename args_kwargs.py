987fc072725e8ad32b078298a352ccb8118763e5
UdamShakya <udamilukpotha@gmail.com>
2026-01-17 23:09:11 +0530

    args nand kwargs

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,6 +1,25 @@
+
+# * unpacking operator is important not the name of the arg
 def add (*args):
     total = 0
     for num in args:
         total += num
     return total
 
+print(add(1,2,3,4))
+
+
+def name (*names):
+    for name in names:
+        print(name,end=" ")
+
+name("udam","shakya","ilukpotha")
+
+
+def print_address(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print_address(street="123 Main St", city="Anytown", state="CA", zip="12345")
+
+ 
