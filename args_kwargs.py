5785628219a22179065d6834e1068540eeaa2d10
UdamShakya <udamilukpotha@gmail.com>
2026-01-17 23:14:13 +0530

    example

--- args_kwargs.py
+++ args_kwargs.py
@@ -22,4 +22,20 @@ def print_address(**kwargs):
 
 print_address(street="123 Main St", city="Anytown", state="CA", zip="12345")
 
- 
+ # arg and kwargs example
+
+def shipping_label(*args,**kwargs):
+    for arg in args:
+        print(arg,end="")
+    print()
+
+    if "apt" in kwargs:
+        print(f"{kwargs.get('street')}:{kwargs.get('apt')}")
+    else:
+        print(f"{kwargs.get('street')}")
+
+    print(f"{kwargs.get('city')}, {kwargs.get('state')} {kwargs.get('zip')}")
+
+shipping_label("Mr.","John","Doe", street="123 Main St", apt="4B", city="Anytown", state="CA", zip="12345")
+
+        
