08f2ef7303cd7d289c9c0317586e5f1e6f235246
UdamShakya <udamilukpotha@gmail.com>
2026-01-18 23:01:13 +0530

    module

---
+++ module.py
@@ -0,0 +1,8 @@
+import math
+#print(help(math))
+
+import numpy as np
+from numpy import minimum #sometimes can create naming confusion 
+
+minimum_value = minimum(4, 10)
+print("Minimum value between 4 and 10 is:", minimum_value)
