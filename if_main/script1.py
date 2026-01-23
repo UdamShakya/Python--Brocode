2335368c5225afbc6e2206191869714442502bb8
UdamShakya <udamilukpotha@gmail.com>
2026-01-23 23:26:26 +0530

    Banking program

--- if_main/script1.py
+++ if_main/script1.py
@@ -2,14 +2,57 @@
 #from script2 import *
 
 #favourite_food()
-def main():
-    favourite_food() 
-    print("this is script1")
+# def main():
+#     favourite_food() 
+#     print("this is script1")
 
-def favourite_food(food):
-    print("My favourite food is", food)
+# def favourite_food(food):
+#     print("My favourite food is", food)
 
 
+#python banking program 
 
+def  show_balance():
+    print(f'Your balance is {balance:,.2f}')
+
+def deposit():
+    amount=float(input("Enter amount to deposit: "))
     
+    if amount< 0:
+         print("Invalid amount. Please enter a positive value.")
+    else:
+         return amount
 
+def withdraw():
+    amount = float(input("Enter amount to withdraw: "))
+    if amount < 0:
+        print("Invalid amount. Please enter a positive value.")
+    elif amount > balance:
+        print("Insufficient funds.")
+    else:
+        return amount
+
+balance = 0
+is_running = True
+
+while is_running:
+    print("-----Banking program-----")
+    print("1. Show Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        show_balance()
+    elif choice == "2":
+        balance += deposit()
+    elif choice == "3":
+        balance -= withdraw()
+    elif choice == "4":
+        is_running = False
+    else:
+            print("Invalid choice. Please try again.")
+
+print("Thank you for using the banking program.")
