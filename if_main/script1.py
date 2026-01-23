7bda4d996c0e5ec4a77067301913ceb7a287c8dc
UdamShakya <udamilukpotha@gmail.com>
2026-01-23 23:06:21 +0530

    .

--- if_main/script1.py
+++ if_main/script1.py
@@ -1,18 +1,15 @@
 #print(dir())
 #from script2 import *
 
-
-
 #favourite_food()
 def main():
     favourite_food() 
     print("this is script1")
 
-def favourite_food():
-    print("My favourite food is pizza.")
+def favourite_food(food):
+    print("My favourite food is", food)
 
-if __name__ == "__main__":
-    print("Success: The main block is working!")
+
 
     
 
