b9b16f127dde471464c94052d769503cba4a2dcf
UdamShakya <udamilukpotha@gmail.com>
2026-01-23 22:50:04 +0530

    .

---
+++ if_main/script2.py
@@ -0,0 +1,10 @@
+from script1 import *
+
+def favourite_drink(drink):
+    print("My favourite drink is", drink)
+
+print ("this is script2")
+favourite_drink("coffee")
+favourite_food()
+
+print("goodbye")
