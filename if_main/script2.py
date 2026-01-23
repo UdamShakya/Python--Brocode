016e19f026361a8aa5ae92f0be6ec233884350ed
UdamShakya <udamilukpotha@gmail.com>
2026-01-23 23:06:30 +0530

    .

--- if_main/script2.py
+++ if_main/script2.py
@@ -3,8 +3,11 @@ from script1 import *
 def favourite_drink(drink):
     print("My favourite drink is", drink)
 
-print ("this is script2")
-favourite_drink("coffee")
-favourite_food()
+def main():
+    print("this is script2")
+    favourite_drink("coffee")
+    favourite_food("pizza")
+    print("goodbye")
 
-print("goodbye")
+if __name__ == "__main__":
+    main()
