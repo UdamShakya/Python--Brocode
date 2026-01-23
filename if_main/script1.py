c7dd94cafa4bafc3e73c702f841a4e788511b8c0
UdamShakya <udamilukpotha@gmail.com>
2026-01-23 23:45:29 +0530

    .

--- if_main/script1.py
+++ if_main/script1.py
@@ -1,13 +1,13 @@
-#print(dir())
-#from script2 import *
+print(dir())
+from script2 import *
 
-#favourite_food()
-# def main():
-#     favourite_food() 
-#     print("this is script1")
 
-# def favourite_food(food):
-#     print("My favourite food is", food)
+def main():
+    favourite_food("Pizza")
+    print("this is script1")
+
+def favourite_food(food):
+    print("My favourite food is", food)
 
 
 #python banking program 
