3dcdcbd3b97423eb92ebca81fd20eab4b27c6f0c
UdamShakya <udamilukpotha@gmail.com>
2026-02-12 23:49:04 +0530

    instance variables

--- OOP/main.py
+++ OOP/main.py
@@ -1,13 +1,16 @@
-from car import car
+from car import car, student
 
 
 car1=car("Toyota",2020,"Red",True)
-car2=car("Honda",2018,"Blue",False)
+car2=car("Honda",2018,"Blue",False) 
+student1= student("John", 20) 
+student2= student("Jane", 22)
+student3= student("Doe", 19)
 
-print(car1.model)
-print(car1.year)
-print(car1.colour)
-print(car1.for_sale)
+print(car1.model) 
+print(car1.year) 
+print(car1.colour) 
+print(car1.for_sale) 
 
 print(car2.model)
 print(car2.year)
@@ -15,3 +18,6 @@ print(car2.colour)
 print(car2.for_sale)
 
 car1.drive()
+print(student1.class_year)
+
+print(student.Num_student)
