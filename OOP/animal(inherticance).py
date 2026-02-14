83337303ba2cabdd7faa625feaacb822c78cab30
UdamShakya <udamilukpotha@gmail.com>
2026-02-14 23:17:56 +0530

    multiple and multilevel inheritance

--- OOP/animal(inherticance).py
+++ OOP/animal(inherticance).py
@@ -2,6 +2,9 @@
 #helps with code reusability and extensibility
 #class Child(Parent)
 
+#mulitple inheritance - inhertifrom more than one parent class C(A,B)
+#muultilevel inheritance - a class can be derived from another derived class
+
 class animal:
     def __init__ (self,name):
         self.name=name
@@ -32,3 +35,43 @@ mouse = mouse("Jerry")
 
 print(dog.name)
 dog.eat()
+
+#multiple inheritance and multilevel inheritance 
+
+class animal:
+    def eat(self):
+        print(f"{self.name} is eating.")
+    def sleep(self):
+        print(f"{self.name} is sleeping.")
+
+class prey(animal):
+    def flee(self):
+        print(f"{self.name} is fleeing.")
+
+class predator(animal):
+    def hunt(self):
+        print(f"{self.name} is hunting.")
+
+class rabbit(prey):
+    def __init__(self, name):
+        self.name = name
+
+class hawk(predator):
+    def __init__(self, name):
+        self.name = name
+    pass
+
+class fish(prey, predator):
+    def __init__(self, name):
+        self.name = name
+
+rab= rabbit("Bunny")
+fishie =fish("Nemo")
+
+fishie.hunt()
+fishie.flee()
+rab.flee()
+
+rab.eat()
+rab.sleep()
+fishie.eat()
