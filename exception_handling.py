06bf2395f91dda9d7698c04f66b587ab7a3ddbba
UdamShakya <udamilukpotha@gmail.com>
2026-02-23 23:52:35 +0530

    exception handling

---
+++ exception_handling.py
@@ -0,0 +1,4 @@
+#exception handling = An event that interrupts the flow of a program
+#try = block of code to test for errors
+#except = block of code that executes in case of an error
+#finally = block of code that executes no matter what
